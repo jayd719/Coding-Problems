4f3770839db3bf008e7a579379ffd61bc196531b
JD <jashansingh29@gmail.com>
2022-07-04 19:33:35 +0530

    Random Code Dump

--- MatchWatch.py
+++ MatchWatch.py
@@ -5,22 +5,26 @@ import time
 
 
 def one():
-    while True:    
-        browser = webdriver.Chrome("/Users/jd/Drive J/Python Projects/chromedriver")
-        browser.get('https://www.sonyliv.com/live-sport/india-tour-of-england-2022-1700000765/cricket-5th-test-day-4-4-jul-2022-1000177435?watch=true')
+    while True:
+        browser = webdriver.Chrome(
+            "/Users/jd/Drive J/Python Projects/chromedriver")
+        browser.get(
+            'https://www.sonyliv.com/live-sport/india-tour-of-england-2022-1700000765/cricket-5th-test-day-4-4-jul-2022-1000177435?watch=true')
         time.sleep(20)
         browser.delete_all_cookies()
         browser.close()
 
+
 def two():
-    browser = webdriver.Chrome("/Users/jd/Drive J/Python Projects/chromedriver")
+    browser = webdriver.Chrome(
+        "/Users/jd/Drive J/Python Projects/chromedriver")
     browser.get('https://www.sonyliv.com/live-sport/india-tour-of-england-2022-1700000765/cricket-5th-test-day-4-4-jul-2022-1000177435?watch=true')
     actions = ActionChains(browser)
     while True:
         time.sleep(300)
         browser.delete_all_cookies()
         browser.get("chrome://settings/clearBrowserData")
-        for i in range(0,7):
+        for i in range(0, 7):
             time.sleep(.25)
             actions.send_keys(Keys.TAB)
             actions.perform()
@@ -28,7 +32,8 @@ def two():
         actions.send_keys(Keys.RETURN)
         actions.perform()
         time.sleep(.5)
-        browser.get('https://www.sonyliv.com/live-sport/india-tour-of-england-2022-1700000765/cricket-5th-test-day-4-4-jul-2022-1000177435?watch=true')
-    
+        browser.get(
+            'https://www.sonyliv.com/live-sport/india-tour-of-england-2022-1700000765/cricket-5th-test-day-4-4-jul-2022-1000177435?watch=true')
+
 
 two()
